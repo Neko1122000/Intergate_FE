abd9fa50892a243d5270a1b7771a40bd2597fa59
ngocpv22 <ngocpv22@ghtk.co>
2021-12-05 15:27:26 +0700

    Refactor front-end

---
+++ src/js/components/google.login/PrivatePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LogoutButton from '../component/LogoutButton'
+
+const PrivatePage = () => {
+    return (
+        <div>
+            <h2> Đây là trang private </h2>
+            <LogoutButton/>
+        </div>
+    );
+};
+
+export default PrivatePage;
