1b02f5ddb84a6d354e435576eadd90d704fde478
honglongle566 <honglongshino6@gmail.com>
2021-12-19 17:17:39 +0700

    Update Frontend n+5

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ const Login = (props) => {
                 <input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} placeholder="Enter password" />
             </div>
             <div className="form-control__submit2" >
-                <button
+                <button className="meo"
                     type="submit" style={{width:'100px',margin:'10px',border:'1px solid gray', borderRadius: '10px'}}>
                     <Link onClick={e => {
                         e.preventDefault();
@@ -27,7 +27,7 @@ const Login = (props) => {
                         });
                     }} style={{textDecoration:'none', color:'black'}} to="/home">Confirm</Link>
                 </button>
-                <button type="submit" style={{width:'300px',border:'1px solid gray',borderRadius: '10px'}} >  
+                <button className="meo" type="submit" style={{width:'300px',border:'1px solid gray',borderRadius: '10px'}} >  
                         <Link style={{textDecoration:'none', color:'black'}} to="/sign-up"> Don't have an account? Sign up!</Link>
                 </button>
             </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,7 +5,7 @@ export default class SignUp extends React.Component {
     render() {
         return (
             <form className="auth-form signup-form">
-                <h3>Register</h3>
+                <h3>Sign Up</h3>
                 <div className="form-group">
                     <label>Username</label>
                     <input type="username" className="form-control" placeholder="Enter username" />
@@ -13,10 +13,10 @@ export default class SignUp extends React.Component {
                     <input type="password" className="form-control" placeholder="Enter password" />
                 </div>
                 <div className="form-control__submit" >
-                    <button type="submit" style={{width:'100px',margin:'10px',border:'1px solid gray', borderRadius: '10px',color:'black'}}>
+                    <button className="meo" type="submit" style={{width:'100px',margin:'10px',border:'1px solid gray', borderRadius: '10px',color:'black'}}>
                         <Link style={{textDecoration:'none', color:'black'}} to="/home">Confirm</Link>
                     </button>
-                    <button type="submit" style={{width:'250px',border:'1px solid gray',borderRadius: '10px'}} >  
+                    <button className="meo" type="submit" style={{width:'250px',border:'1px solid gray',borderRadius: '10px'}} >  
                         <Link style={{textDecoration:'none', color:'black'}} to="/sign-in">Do have an account?Log in</Link>
                     </button>
                 </div>
